cddddacd319b973f485a13b5da46cdc1f33d0b29
Elias Neto <93806852+Elias-Neto@users.noreply.github.com>
2023-06-06 12:39:21 -0300

    add new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,16 +3,32 @@ const router = express.Router()
 
 const genericRoutes = require('./genericRoutes')
 
-const { ordersController, officesController, customersController } = require("../controllers")
+const {
+  ordersController,
+  officesController,
+  productsController,
+  paymentsController,
+  customersController,
+  orderDetailsController,
+  productsLineController
+} = require("../controllers")
 
 const employeesRouter = require('./employeesRoutes')
 const ordersRouter = genericRoutes(ordersController)
 const officesRouter = genericRoutes(officesController)
+const productsRouter = genericRoutes(productsController)
+const paymentsRouter = genericRoutes(paymentsController)
 const customersRouter = genericRoutes(customersController)
+const orderDetailsRouter = genericRoutes(orderDetailsController)
+const productsLineRouter = genericRoutes(productsLineController)
 
 router.use('/orders', ordersRouter)
 router.use('/offices', officesRouter)
-router.use('/employees', employeesRouter)
+router.use('/products', productsRouter)
+router.use('/payments', paymentsRouter)
 router.use('/customers', customersRouter)
+router.use('/employees', employeesRouter)
+router.use('/orderdetails', orderDetailsRouter)
+router.use('/productsLine', productsLineRouter)
 
 module.exports = router
