e6831b9d9c7a5aa2987179da940a84f7ab44486e
Elias Neto <93806852+Elias-Neto@users.noreply.github.com>
2023-06-06 12:39:14 -0300

    add relationships

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,46 @@
 const sequelize = require('./sequelize')
 
+const envIsDevelopment = process.env.NODE_ENV === 'development'
+
 // Models
 
-const officeModel = require('../models/officesModel')
-const employeeModel = require('../models/employeesModel')
+const ordersModel = require('../models/ordersModel')
+const officesModel = require('../models/officesModel')
+const paymentsModel = require('../models/paymentsModel')
+const productsModel = require('../models/productsModel')
+const employeesModel = require('../models/employeesModel')
+const customersModel = require('../models/customersModel')
+const productLinesModel = require('../models/productLinesModel')
+const orderDetailsModel = require('../models/orderDetailsModel')
 
 // RELATIONSHIPS
 
 // Office has Many Employees
-officeModel.hasMany(employeeModel, { foreignKey: 'officeCode' })
-employeeModel.belongsTo(officeModel, { foreignKey: 'officeCode' })
+officesModel.hasMany(employeesModel, { foreignKey: 'officeCode', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+employeesModel.belongsTo(officesModel, { foreignKey: 'officeCode', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
 
 // Employee has Many Employees
-employeeModel.hasMany(employeeModel, { foreignKey: 'reportsTo' })
-employeeModel.belongsTo(employeeModel, { foreignKey: 'reportsTo' })
+employeesModel.hasMany(employeesModel, { foreignKey: 'reportsTo', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+employeesModel.belongsTo(employeesModel, { foreignKey: 'reportsTo', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
 
-sequelize.sync({ force: true }) 
+// Employee has Many Customers
+employeesModel.hasMany(customersModel, { foreignKey: 'salesRepEmployeeNumber', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+customersModel.belongsTo(employeesModel, { foreignKey: 'salesRepEmployeeNumber', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+
+// Customer has One Payments
+customersModel.hasOne(paymentsModel, { foreignKey: 'customerNumber', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+paymentsModel.belongsTo(customersModel, { foreignKey: 'customerNumber', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+
+// Customer has Many Orders
+customersModel.hasMany(ordersModel, { foreignKey: 'customerNumber', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+ordersModel.belongsTo(customersModel, { foreignKey: 'customerNumber', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+
+// ProductLine has Many Products
+productLinesModel.hasMany(productsModel, { foreignKey: 'productLine', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+productsModel.belongsTo(productLinesModel, { foreignKey: 'productLine', as: "productLineName", onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+
+// Product has Many Orders and Orders has Many Products
+productsModel.belongsToMany(ordersModel, { through: orderDetailsModel, foreignKey: 'productCode', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+ordersModel.belongsToMany(productsModel, { through: orderDetailsModel, foreignKey: 'orderNumber', onUpdate: "NO ACTION", onDelete: "NO ACTION" })
+
+sequelize.sync({ force: envIsDevelopment }) 
